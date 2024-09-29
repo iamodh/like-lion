b3dfa2da463ec42abe99c67ae6a9465294c8b6a2
Donghwan Oh <imodh97@gmail.com>
2024-09-29 11:20:13 +0900

    완성

--- javascript/day15/homework/main.js
+++ javascript/day15/homework/main.js
@@ -3,15 +3,23 @@ function Product(name, price) {
   this.price = price;
 }
 
-let products = [
+const products = [
   new Product("대뱃살", 3000),
   new Product("목살", 5000),
   new Product("배꼽살", 4000),
   new Product("중뱃살", 1000),
 ];
 
+// 선택된 상품을 담는 배열
+let bag = [];
+
+// bag 목록의 총 가격
+// opener에서 참조를 위해 var 키워드 사용
+var total = 0;
+
 const $options = document.getElementById("options");
 const $result = document.getElementById("result");
+const $button = document.getElementById("button");
 
 // js 파일 로드 시에 products를 화면에 표시
 window.addEventListener("load", () => {
@@ -23,10 +31,8 @@ window.addEventListener("load", () => {
   });
 });
 
-const selectProducts = function () {
-  // 선택된 상품을 담는 배열
-  let bag = [];
-
+const selectProducts = function (bag) {
+  bag = [];
   const selectedProducts = [...$options.querySelectorAll("option:checked")];
   selectedProducts.forEach((product) => {
     // 선택된 product를 bag에 push한다.
@@ -51,11 +57,10 @@ const printBag = function (bag) {
   });
 };
 
-// total에 bag 목록의 총 가격을 출력한다.
-
+// total에 bag 목록의 총 가격을 출력하는 함수
 const printTotal = function (bag) {
   const $total = document.getElementById("total");
-  let total = 0;
+  total = 0;
 
   bag.forEach((item) => {
     total += item.price;
@@ -66,10 +71,28 @@ const printTotal = function (bag) {
 
 $options.addEventListener("change", (e) => {
   $result.textContent = "";
-  const bag = selectProducts();
+  bag = selectProducts(bag);
 
   if (bag.length > 0) {
     printBag(bag);
     printTotal(bag);
   }
 });
+
+$button.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (bag.length === 0) {
+    alert("결재할 상품을 선택해야 합니다.");
+  } else {
+    window.open(
+      "./payment.html",
+      "_blank",
+      "left=100,top=100,width=400,height=300"
+    );
+  }
+});
+
+// child에서 실행되는 함수
+function checkPayment(cardNumber) {
+  alert(`${cardNumber}로 ${total}원 결재 완료되었습니다.`);
+}
